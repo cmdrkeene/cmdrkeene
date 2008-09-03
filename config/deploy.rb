e793529bc7943c624481bf446df507e83a8456df
Brandon Keene <bkeene@gmail.com>
2008-09-03 00:25:11 -0700

    More hello world shennangins

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,8 @@
 set :user, "deploy" # make sure you are key authenticated
+set :group, "deploy"
 set :application, "cmdrkeene"
+set :mongrel_user, "deploy"
+set :runner, "deploy"
 
 # :deploy_to => /opt/apps/#{application} by default
 set :repository, "git@github.com:cmdrkeene/cmdrkeene.git"
@@ -9,3 +12,9 @@ set :deploy_via, :remote_cache
 role :app, "cmdrkeene.com"
 role :web, "cmdrkeene.com"
 role :db,  "cmdrkeene.com", :primary => true
+
+namespace :deploy do
+  task :restart do
+    deprec.mongrel.restart
+  end
+end
